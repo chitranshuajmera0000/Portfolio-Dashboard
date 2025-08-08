f368c0a2bc79ad7b1cff9be4ea4e29874733da1f
chitranshuajmera0000 <ajmerachitranshu951@gmail.com>
2025-08-08 14:37:39 +0530

    PORTFOLIO UPDATED

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { usePortfolioData } from '../hooks/usePortfolioData';
 import { HoldingsTable } from '../components/HoldingsTable';
